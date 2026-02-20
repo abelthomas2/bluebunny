3a1eae0c8974dd4fa86ee7bac3d87c883fcaf7c9
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2026-02-19 19:04:03 -0500

    mobile text

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
 
       <div className="relative z-10 flex min-h-screen w-full items-center justify-center px-5 pt-32 pb-20 md:mt-[var(--navbar-height)] md:min-h-[calc(100vh-var(--navbar-height))] md:flex-col md:pt-0 md:pb-0">
         <div className="w-full text-center">
-          <h1 className="text-5xl md:text-7xl font-semibold !text-white mb-3">
+          <h1 className="text-4xl md:text-7xl font-semibold !text-white mb-3">
             Orlando&rsquo;s <span className="!text-[#5DAFD5]">#1</span>
             <br />
             STR Turnover Service
